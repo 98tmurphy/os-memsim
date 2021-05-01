06453aac7a5981746c1ec92031ed925ad19b1030
boro8026 <71035732+boro8026@users.noreply.github.com>
2021-04-30 20:03:08 -0500

    Update mmu.cpp

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -4,6 +4,7 @@ Mmu::Mmu(int memory_size)
 {
     _next_pid = 1024;
     _max_size = memory_size;
+    memoryUsed = 0;
 }
 
 Mmu::~Mmu()
@@ -45,6 +46,9 @@ void Mmu::addVariableToProcess(uint32_t pid, std::string var_name, DataType type
     var->type = type;
     var->virtual_address = address;
     var->size = size;
+    if(var_name != "<FREE_SPACE>"){
+        memoryUsed += size;
+    }
     if (proc != NULL)
     {
         proc->variables.push_back(var);
@@ -88,6 +92,11 @@ void Mmu::print()
 void Mmu::removeProcess(uint32_t pid){
     for (int i = 0; i < _processes.size(); i++){
         if(_processes[i]->pid == pid){
+            for(int j = 0; j < _processes[i]->variables.size(); j++){
+                if(_processes[i]->variables[j]->name != "<FREE_SPACE>"){
+                    memoryUsed = memoryUsed - _processes[i]->variables[j]->size;
+                }
+            }
             _processes.erase(_processes.begin()+i);
         }
     }
@@ -105,17 +114,19 @@ void Mmu::removeFirstFree(uint32_t pid){
 }
 
 //Replaces a free space with a new variable and 
-void Mmu::removeVariable(uint32_t pid, std::string var, uint32_t virtualAddr){
+void Mmu::removeVariable(uint32_t pid, std::string var){
     for (int i = 0; i < _processes.size(); i++){
         if(_processes[i]->pid == pid){
             uint32_t size = _processes[i]->variables.size();
             for(int j = 0; j < size; j++){
                 if(_processes[i]->variables[j]->name == "<FREE_SPACE>" && _processes[i]->variables[j]->size == 0){
                     _processes[i]->variables.erase(_processes[i]->variables.begin() + j);
+                    j--;
                 }
                 else if(_processes[i]->variables[j]->name == var){
                     _processes[i]->variables[j]->name = "<FREE_SPACE>";
                     _processes[i]->variables[j]->type = DataType::FreeSpace;
+                    memoryUsed = memoryUsed - _processes[i]->variables[j]->size;
                 }
             }
         }
@@ -195,7 +206,9 @@ uint32_t Mmu::getVirAddressOfFreeSpace(uint32_t pid,uint32_t sizeOfVariable){
     return virAddress;
 }
 
+//Will input the new variable into the free space, and fill left over space with a free space
 uint32_t Mmu::removeFreeSpace(uint32_t pid,uint32_t virMemAddr,uint32_t sizeOfVariable, std::string var_name, DataType type){
+    memoryUsed += sizeOfVariable;
     std::vector<Variable*> newVariables;
     for(int i = 0; i < _processes.size(); i++){
         if(_processes[i]->pid == pid){
@@ -252,4 +265,51 @@ uint32_t Mmu::getLargestVirtualAddress(uint32_t pid){
         }
     }
     return maxSoFar;
+}
+
+bool Mmu::containsPid(uint32_t pid){
+    for(int index = 0; index < _processes.size(); index++){
+        if(_processes[index]->pid == pid){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Mmu::spaceInMemory(uint32_t size){
+    return memoryUsed + size <= _max_size;
+}
+
+std::vector<uint32_t> Mmu::getFreeSpaceRanges(uint32_t pid){
+    std::vector<uint32_t> result;
+    uint32_t holdVirAddr;
+    uint32_t offset = 1;
+    for(int i = 0; i < _processes.size(); i++){
+        if(_processes[i]->pid == pid){
+            for(int j = 0; j < _processes[i]->variables.size(); j++){
+                if(_processes[i]->variables[j]->name == "<FREE_SPACE>" && j != _processes[i]->variables.size()-1){
+                    result.push_back(_processes[i]->variables[j]->virtual_address);
+                    holdVirAddr = _processes[i]->variables[j]->virtual_address + (_processes[i]->variables[j]->size - 1);
+                    while(j + offset < _processes[i]->variables.size()){
+                        if(_processes[i]->variables[j+offset]->name == "<FREE_SPACE>"){
+                            if(j+offset == _processes[i]->variables.size() - 1){
+                                holdVirAddr += 32768;
+                            }
+                            else{
+                                holdVirAddr += _processes[i]->variables[j+offset]->size;
+                            }
+                            offset++;
+                        }
+                        else{
+                            offset = _processes[i]->variables.size();
+                        }
+                    }
+                    result.push_back(holdVirAddr);
+                    j += (offset - 1);
+                    offset = 1;
+                }
+            }
+        }
+    }
+    return result;
 }
